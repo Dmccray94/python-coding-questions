65e8d0c3fb355093d9f5586f3f92e6fac6fe94b7
Dmccray94 <dantemmccray@gmail.com>
2022-07-22 22:26:24 -0700

    python HW questions

---
+++ index.py
@@ -0,0 +1,35 @@
+# question 1
+
+def username(yougotitdude):
+    f'username == {yougotitdude}'
+    input = 'Hello_unsername'
+print(username)
+
+
+#question 2
+def listOfoddnumbers(x):
+    if x % 2 == 0:
+        x = x + 1
+        odd = list(range(1,1000))
+    return odd
+print(listOfoddnumbers(15))
+
+
+#question 3
+intList = [1,17,6,27,89]
+def highestNumber(lst):
+    print(max(lst))
+highestNumber(intList)
+## question 4
+def is_leap_year(leap):
+    if leap % 4 == 0: 
+        return True if leap % 100 == 0 
+    return False if leap % 4 == 
+    return False
+    print (leap_year (1908))
+    print (leap_year (2020))
+
+# question 5
+
+# def is_consecutive(a_list):
+    # if range()
